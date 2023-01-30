78868c856ef5d3260139e3893f95f16c8cf4b008
Yang, Bo <pop.atry@gmail.com>
2023-01-30 02:31:41 -0800

    Add Scaladoc

--- build.sbt
+++ build.sbt
@@ -1,4 +1,6 @@
 enablePlugins(ScalaJSPlugin)
+enablePlugins(ScalaJSBundlerPlugin)
+enablePlugins(Example)
 
 ThisBuild / organization := "com.yang-bo"
 
@@ -6,9 +8,9 @@ name := "ReactBinding"
 
 libraryDependencies += {
   if (VersionNumber(scalaJSVersion).matchesSemVer(SemanticSelector(">=1"))) {
-     "me.shadaj" %%% "slinky-core" % "0.7.3"
+    "me.shadaj" %%% "slinky-core" % "0.7.3"
   } else {
-     "me.shadaj" %%% "slinky-core" % "0.6.8"
+    "me.shadaj" %%% "slinky-core" % "0.6.8"
   }
 }
 
@@ -21,3 +23,29 @@ libraryDependencies += {
     "com.thoughtworks.binding" %%% "binding" % "11.9.0"
   }
 }
+
+libraryDependencies += "org.scalatest" %%% "scalatest" % "3.2.15" % Test
+
+libraryDependencies += {
+  if (VersionNumber(scalaJSVersion).matchesSemVer(SemanticSelector(">=1"))) {
+    "me.shadaj" %%% "slinky-testrenderer" % "0.7.3" % Test
+  } else {
+    "me.shadaj" %%% "slinky-testrenderer" % "0.6.8" % Test
+  }
+}
+
+libraryDependencies += {
+  if (VersionNumber(scalaJSVersion).matchesSemVer(SemanticSelector(">=1"))) {
+    "me.shadaj" %%% "slinky-web" % "0.7.3" % Test
+  } else {
+    "me.shadaj" %%% "slinky-web" % "0.6.8" % Test
+  }
+}
+
+webpack / version := "5.75.0"
+
+Test / requireJsDomEnv := true
+
+Test / npmDependencies += "react-dom" -> "18.2.0"
+
+Test / npmDependencies += "react-test-renderer" -> "18.2.0"
--- src/main/scala/com/yang_bo/ReactBinding.scala
+++ src/main/scala/com/yang_bo/ReactBinding.scala
@@ -5,6 +5,67 @@ import slinky.core.facade._
 
 import scala.language.implicitConversions
 import scala.scalajs.js
+
+/** A React Component backed by a `Binding[ReactElement]`.
+  *
+  * @example
+  *   With the help of [[ImplicitConversions]],
+  *   [[com.thoughtworks.binding.Binding]] can be used as an React element.
+  *
+  *   The following code create a spinner, which includes a label whose value is
+  *   automatically updated according to `currentNumber`:
+  *   {{{
+  *   import com.thoughtworks.binding.Binding
+  *   import com.thoughtworks.binding.Binding.Var
+  *   import com.yang_bo.ReactBinding.ImplicitConversions._
+  *   import slinky.web.html._
+  *   import slinky.core.facade._
+  *
+  *   def spinner(currentNumber: Var[Int]) = {
+  *     div(
+  *       button(id := "minus", onClick := { e => currentNumber.value -= 1 })(
+  *         "-"
+  *       ),
+  *       // With the help of `ImplicitConversions`, `Binding` can be used as an
+  *       // React element
+  *       Binding {
+  *         // `.bind` is allowed in the Binding block
+  *         label(currentNumber.bind.toString)
+  *       },
+  *       button(id := "plus", onClick := { e => currentNumber.value += 1 })(
+  *         "+"
+  *       ),
+  *     )
+  *   }
+  *   }}}
+  *   The component can be rendered and respond to UI events
+  *   {{{
+  *
+  *   import slinky.web.ReactDOM
+  *   import slinky.testrenderer.TestRenderer
+  *   import org.scalajs.dom._
+  *   import org.scalajs.dom.raw._
+  *   val currentNumber = Var(50)
+  *   TestRenderer.act { () =>
+  *     ReactDOM.render(spinner(currentNumber), document.body)
+  *   }
+  *   currentNumber.value should be(50)
+  *   document.body.innerHTML should be(
+  *     """<div><button id="minus">-</button><label>50</label><button id="plus">+</button></div>"""
+  *   )
+  *
+  *   TestRenderer.act { () =>
+  *     document.getElementById("minus").dispatchEvent(new Event("click", new EventInit {
+  *       bubbles = true
+  *       cancelable = true
+  *     }))
+  *   }
+  *   currentNumber.value should be(49)
+  *   document.body.innerHTML should be(
+  *     """<div><button id="minus">-</button><label>49</label><button id="plus">+</button></div>"""
+  *   )
+  *   }}}
+  */
 object ReactBinding extends ComponentWrapper {
   type Props = Binding[ReactElement]
   type State = ReactElement
@@ -27,7 +88,16 @@ object ReactBinding extends ComponentWrapper {
     }
   }
 
-  object ImplicitConversions {
+  private[ReactBinding] trait LowPriorityImplicitConversions1024 {
+    @inline implicit def bindingToReactElement[From](
+        binding: Binding[From]
+    )(implicit toReactElement: From => ReactElement): ReactElement =
+      ReactBinding(
+        binding.map(toReactElement)
+      )
+  }
+
+  object ImplicitConversions extends LowPriorityImplicitConversions1024 {
     @inline implicit def bindingReactElementToReactElement(
         binding: Binding[ReactElement]
     ): ReactElement = ReactBinding(binding)
